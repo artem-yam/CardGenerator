024a6f719cbfc9debf0b3a4bcf223182fb683766
Artemiy Yamshchikov <Artemiy_Yamshchikov@epam.com>
2018-11-06 16:08:35 +0300

    Initial commit: added project main class + .gitignore

---
+++ src/com/company/CardGenerator.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Class for generating valid cards numbers depending on input cards types
+ */
+public class CardGenerator {
+
+    /**
+     * Method for generating valid cards numbers depending on input cards types
+     *
+     * @param args - String with cards types
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
